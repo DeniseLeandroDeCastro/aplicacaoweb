1058405e7680c022a377be09aec2c00d446a933c
DeniseLeandroDeCastro <dmaria.natal@gmail.com>
2021-10-10 21:23:05 -0300

    Projeto criado

---
+++ src/main/java/com/denisecastro/aplicacaoweb/AplicacaowebApplication.java
@@ -0,0 +1,13 @@
+package com.denisecastro.aplicacaoweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AplicacaowebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AplicacaowebApplication.class, args);
+	}
+
+}
